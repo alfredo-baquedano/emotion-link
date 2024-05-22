7266c7f0a6f9e91ee34cfba54b2700d9547cf7a9
abaquedano <abaqueda@alumnos.ubiobio.cl>
2024-05-22 04:59:43 -0600

    translate error and not found pages

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,19 +1,19 @@
-import { Link as RouterLink } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import { Link as RouterLink } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const ErrorPage = () => {
   return (
-    <Box sx={{ textAlign: "center", marginTop: "50px" }}>
-      <Typography variant="h4" gutterBottom>
+    <Box sx={{ textAlign: 'center', marginTop: '50px' }}>
+      <Typography variant='h4' gutterBottom>
         ERROR
       </Typography>
-      <Typography variant="body1" gutterBottom>
-        Se ha producido un error inesperado.
+      <Typography variant='body1' gutterBottom>
+        An unexpected error has occurred.
       </Typography>
-      <Button variant="text" color="primary" component={RouterLink} to="/">
-        Ir al inicio
+      <Button variant='text' color='primary' component={RouterLink} to='/'>
+        Return to Home
       </Button>
     </Box>
   );
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,19 +1,19 @@
-import { Link as RouterLink } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import { Link as RouterLink } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const NotFound = () => {
   return (
-    <Box sx={{ textAlign: "center", marginTop: "50px" }}>
-      <Typography variant="h4" gutterBottom>
+    <Box sx={{ textAlign: 'center', marginTop: '50px' }}>
+      <Typography variant='h4' gutterBottom>
         404 ERROR
       </Typography>
-      <Typography variant="body1" gutterBottom>
-        La página que buscas no existe
+      <Typography variant='body1' gutterBottom>
+        The page you are looking for does not exist
       </Typography>
-      <Button variant="text" color="primary" component={RouterLink} to="/">
-        Ir al inicio
+      <Button variant='text' color='primary' component={RouterLink} to='/'>
+        Return to home
       </Button>
     </Box>
   );
